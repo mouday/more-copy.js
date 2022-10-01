8dcfa9fb22d14b11b917bf6150309acfd8fb501b
pengshiyu <1940607002@qq.com>
2022-10-01 18:05:36 +0800

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 // 命令行
-const fs = require("fs");
-const path = require("path");
-const { Command } = require("commander");
-const { VERSION } = require("./version.js");
+const fs = require('fs')
+const path = require('path')
+const { Command } = require('commander')
+const { VERSION } = require('./version.js')
 // const { mergeConfig } = require("./utils/merge-config.js");
-const { moreCopy } = require("./index.js");
-const defaultConfig = require("./default.config.js");
+const { moreCopy } = require('./index.js')
+const defaultConfig = require('./default.config.js')
 
 // 读取自定义配置
 // const default_config_filename = "./more-copy.config.js";
-const CUSTOM_CONFIG_FILENAME = "./more-copy.config.js";
+const CUSTOM_CONFIG_FILENAME = './more-copy.config.js'
+const CUSTOM_CONFIG_FILENAME_CJS = './more-copy.config.cjs'
 
 /**
  * 获取命令行参数
@@ -19,12 +20,12 @@ const CUSTOM_CONFIG_FILENAME = "./more-copy.config.js";
  */
 function getCommandOptions() {
   // console.log(process.argv);
-  const program = new Command();
+  const program = new Command()
 
-  program.version(VERSION);
+  program.version(VERSION)
 
-  program.name("mcp");
-  program.usage("[options]");
+  program.name('mcp')
+  program.usage('[options]')
   // program.option("-i, --input <input filename>", "input filename");
   // program.option("-o, --output <output filename>", "output filename");
   // program.option("-d, --data <json data or data filename>", "extra data", "{}");
@@ -35,39 +36,47 @@ function getCommandOptions() {
   // );
   // program.option("-f, --force", "overwrite output file");
 
-  program.parse(process.argv);
+  program.parse(process.argv)
 
-  let options = program.opts();
+  let options = program.opts()
 
-  return options;
+  return options
 }
 
 // 读取用户配置
-function readCustomConfig() {
-  let custom_config_absolute = path.join(process.cwd(), CUSTOM_CONFIG_FILENAME);
+async function readCustomConfig() {
+  let custom_config_absolute = path.join(process.cwd(), CUSTOM_CONFIG_FILENAME)
+  let custom_config_absolute_cjs = path.join(
+    process.cwd(),
+    CUSTOM_CONFIG_FILENAME_CJS
+  )
 
-  let customConfig = null;
+  let customConfig = null
   if (fs.existsSync(custom_config_absolute)) {
-    customConfig = require(custom_config_absolute);
+    customConfig = require(custom_config_absolute)
+  } else if (fs.existsSync(custom_config_absolute_cjs)) {
+    customConfig = require(custom_config_absolute_cjs)
   }
+  
+  // console.log(customConfig);
 
-  return customConfig;
+  return customConfig
 }
 /**
  * 处理命令行输入的参数
  * @param {*} options
  * @returns
  */
-function getConfig() {
+async function getConfig() {
   // 处理config
   // let config = {};
 
-  let customConfig = readCustomConfig();
+  let customConfig = await readCustomConfig()
 
   if (customConfig) {
-    return { ...defaultConfig, ...customConfig };
+    return { ...defaultConfig, ...customConfig }
   } else {
-    return defaultConfig;
+    return defaultConfig
   }
 
   // console.log(config);
@@ -87,15 +96,15 @@ function getConfig() {
   //   }
   // }
 
-  return options;
+  return options
 }
 
 // 命令行执行入口
-(async () => {
+;(async () => {
   try {
-    let options = getCommandOptions();
-    
-    let config = getConfig();
+    let options = getCommandOptions()
+
+    let config = await getConfig()
 
     // // 收集有效参数
     // let config = {
@@ -105,7 +114,7 @@ function getConfig() {
     //   plugins: options.config.plugins,
     // };
 
-    // console.log(config);
+    console.log(config);
 
     // 参数校验
     // if (!config.input || !config.output) {
@@ -117,8 +126,6 @@ function getConfig() {
     //   throw new Error(`input file not exists: ${config.input}`);
     // }
 
-    
-
     // 目标文件已存在抛出异常
     // if (!options.force) {
     //   if (fs.existsSync(config.output)) {
@@ -127,9 +134,9 @@ function getConfig() {
     // }
 
     // 执行核心逻辑
-    await moreCopy(config);
+    await moreCopy(config)
   } catch (e) {
-    console.error(e);
-    process.exit(-1);
+    console.error(e)
+    process.exit(-1)
   }
-})();
+})()
