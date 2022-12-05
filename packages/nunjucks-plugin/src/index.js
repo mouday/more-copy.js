3ec77ad30b370eba39137a41f8eb196cdfed127f
pengshiyu <1940607002@qq.com>
2022-12-05 15:10:49 +0800

    fix

--- packages/nunjucks-plugin/src/index.js
+++ packages/nunjucks-plugin/src/index.js
@@ -1,5 +1,6 @@
 const nunjucks = require('nunjucks')
 const fs = require('fs')
+const path = require('path')
 
 /**
  * nunjucks模板渲染
@@ -12,12 +13,20 @@ class NunjucksPlugin {
   apply(data) {
     let input = this.options.input
     let output = this.options.output
+    let outputDir = path.parse(output).dir
+    // console.log('outputDir', outputDir);
 
+    // 存在不覆盖
     if (fs.existsSync(output)) {
       console.warn('NunjucksPlugin warn: output file exists', output)
     } else {
       let result = nunjucks.render(input, data)
-    
+
+      // 自动创建输入目录
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true })
+      }
+
       fs.writeFileSync(output, result)
     }
   }
